2bd704935f380b9c458b527f2185bb2384b1bb50
ParallelLines <tishkane@yandex.ru>
2024-09-01 18:06:08 +0200

    front: new icons

---
+++ front/just-react/src/components/IconLogOut.js
@@ -0,0 +1,8 @@
+export default function IconLogOut() {
+    return (
+        <svg className="svg-icon" viewBox="0 0 32 32">
+            <path d="M14 7.63636L14 4.5C14 4.22386 13.7761 4 13.5 4L4.5 4C4.22386 4 4 4.22386 4 4.5L4 19.5C4 19.7761 4.22386 20 4.5 20L13.5 20C13.7761 20 14 19.7761 14 19.5L14 16.3636" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M10 12L21 12M21 12L18.0004 8.5M21 12L18 15.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    )
+}
