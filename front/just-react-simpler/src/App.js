d0055e839c6613bc9beb7e9b5e757eb656e79927
ParallelLines <tishkane@yandex.ru>
2025-10-07 12:07:35 +0200

    front-simpler: some cleaning and a bit of styling

--- front/just-react-simpler/src/App.js
+++ front/just-react-simpler/src/App.js
@@ -4,14 +4,15 @@ import Login from './components/Login'
 import RegularExpensesList from './components/RegularExpensesList'
 import { useCookies } from 'react-cookie'
 
+
 const COOKIE_AUTH_NAME = process.env.REACT_APP_COOKIE_AUTH_NAME
 
+
 export default function App() {
-  const [cookies, setCookie, removeCookie] = useCookies()
+  const [cookies] = useCookies()
   const authToken = cookies[COOKIE_AUTH_NAME]
-  console.log(authToken)
-  console.log(COOKIE_AUTH_NAME)
   return (
+
     <div className="app">
       {!authToken && <Login />}
       {authToken &&
@@ -21,5 +22,6 @@ export default function App() {
           <CategoriesList />
         </div>}
     </div>
+
   )
 }
