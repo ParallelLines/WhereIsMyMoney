76de001fae6cbd2eccbd20ed4d03df96aab17b58
ParallelLines <tishkane@yandex.ru>
2025-10-07 12:06:01 +0200

    front-simpler: +react-error-boundary, moved all the providers and wrappers to index.js so the app.js looks cleener, fixed the cookies

---
+++ front/just-react-simpler/src/components/Error.js
@@ -0,0 +1,9 @@
+export default function Error() {
+    return (
+        <div className="error">
+            Wow, something's not right with this app.<br />
+            We're fixing it.<br />
+            Bitte geduld. Genau
+        </div>
+    )
+}
--- front/just-react-simpler/src/index.js
+++ front/just-react-simpler/src/index.js
@@ -4,12 +4,21 @@ import './reset.css'
 import './index.css'
 import App from './App'
 import { CookiesProvider } from 'react-cookie'
+import { ErrorBoundary } from 'react-error-boundary'
+import Error from './components/Error'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const queryClient = new QueryClient()
+
 root.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </React.StrictMode>
+    <ErrorBoundary fallback={<Error />}>
+      <CookiesProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider >
+      </CookiesProvider>
+    </ErrorBoundary>
+  </React.StrictMode >
 )
