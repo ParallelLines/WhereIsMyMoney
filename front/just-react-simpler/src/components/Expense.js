89df1bc6fe3d879e06f702afdb9c28737d34f17e
ParallelLines <tishkane@yandex.ru>
2025-10-09 16:03:16 +0200

    front-simpler: tooltip for the expense name

--- front/just-react-simpler/src/components/Expense.js
+++ front/just-react-simpler/src/components/Expense.js
@@ -5,7 +5,7 @@ export default function Expense({ data }) {
     return (
         <div className="expense">
             <input type="checkbox"></input>
-            <span className="expense-name">{data.name}</span>
+            <span className="expense-name" title={data.name}>{data.name}</span>
             <span className="expense-sum">{data.sum}</span>
             <span className="expense-currency">{data.symbol}</span>
             <span className="expense-date" title={dateTimeString(data.date)}>{dateString(data.date)}</span>
