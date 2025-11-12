3b18b8dceb5f66bf313b91d8eeb31b3e37b0a6f0
ParallelLines <tishkane@yandex.ru>
2025-11-12 15:13:37 +0100

    back: of course the expenses of child categories should also be included in the result

--- back/express/sql/expensesGetAllByCategory.sql
+++ back/express/sql/expensesGetAllByCategory.sql
@@ -3,6 +3,8 @@ ca.color, e.sum, e.currency, cu.symbol, e.date, e.regular_id, e.regular_name
 FROM expenses e 
 LEFT JOIN categories ca ON e.category_id = ca.id 
 JOIN currencies cu ON e.currency = cu.name 
-WHERE e.user_id = $1 AND e.category_id = $2
+WHERE e.user_id = $1 
+    AND e.category_id = $2 
+    OR (e.category_id = ca.id AND ca.parent_id = $2 )
 ORDER BY e.date DESC
 LIMIT $3 OFFSET $4
