9b525ae88176511e614e879a94b1ed55b179661f
ParallelLines <tishkane@yandex.ru>
2025-12-11 15:48:36 +0100

    back: properly recursive requests for expenses and regulars by category

--- back/express/sql/expensesGetAllByCategory.sql
+++ back/express/sql/expensesGetAllByCategory.sql
@@ -1,10 +1,25 @@
-SELECT e.id, e.user_id, e.category_id, e.name, ca.name AS category_name, 
-ca.color, e.sum, e.currency, cu.symbol, e.date, e.regular_id
-FROM expenses e 
-LEFT JOIN categories ca ON e.category_id = ca.id 
+WITH RECURSIVE category_tree AS (
+  SELECT id, name, color FROM categories WHERE id = $2
+  UNION ALL
+  SELECT c.id, c.name, c.color
+  FROM categories c
+  INNER JOIN category_tree ct ON c.parent_id = ct.id
+)
+SELECT DISTINCT 
+  e.id, 
+  e.user_id, 
+  e.category_id, 
+  e.name, 
+  ct.name AS category_name, 
+  ct.color, 
+  e.sum, 
+  e.currency, 
+  cu.symbol, 
+  e.date, 
+  e.regular_id
+FROM expenses e
+INNER JOIN category_tree ct ON e.category_id = ct.id
 JOIN currencies cu ON e.currency = cu.name 
-WHERE e.user_id = $1 
-    AND e.category_id = $2 
-    OR (e.category_id = ca.id AND ca.parent_id = $2)
+WHERE e.user_id = $1
 ORDER BY e.date DESC
 LIMIT $3 OFFSET $4
--- back/express/sql/regularsGetAllByCategory.sql
+++ back/express/sql/regularsGetAllByCategory.sql
@@ -1,11 +1,32 @@
-SELECT r.id, r.user_id, r.category_id, r.name, ca.name AS category_name, 
-ca.color, r.sum, r.currency, cu.symbol, r.start_date, r.end_date, r.next_date, 
-r.repeat_interval, r.repeat_every, r.repeat_each_weekday, r.repeat_each_day_of_month, 
-r.repeat_each_month, r.repeat_on_day_num, r.repeat_on_weekday 
+WITH RECURSIVE category_tree AS (
+  SELECT id, name, color FROM categories WHERE id = $2
+  UNION ALL
+  SELECT c.id, c.name, c.color
+  FROM categories c
+  INNER JOIN category_tree ct ON c.parent_id = ct.id
+)
+SELECT DISTINCT 
+    r.id, 
+    r.user_id, 
+    r.category_id, 
+    r.name, 
+    ct.name AS category_name, 
+    ct.color, 
+    r.sum, 
+    r.currency, 
+    cu.symbol, 
+    r.start_date, 
+    r.end_date, 
+    r.next_date, 
+    r.repeat_interval, 
+    r.repeat_every, 
+    r.repeat_each_weekday, 
+    r.repeat_each_day_of_month, 
+    r.repeat_each_month, 
+    r.repeat_on_day_num, 
+    r.repeat_on_weekday 
 FROM regulars r
-LEFT JOIN categories ca ON r.category_id = ca.id 
+INNER JOIN category_tree ct ON r.category_id = ct.id
 JOIN currencies cu ON r.currency = cu.name 
-WHERE r.user_id = $1 
-    AND r.category_id = $2 
-    OR (r.category_id = ca.id AND ca.parent_id = $2)
+WHERE r.user_id = $1
 ORDER BY r.start_date DESC
