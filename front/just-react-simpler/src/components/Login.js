00194dae3529f1649753dd45a97532a2774777c6
ParallelLines <tishkane@yandex.ru>
2026-01-01 20:53:15 +0100

    front: + an ability to forbid signing up + auth form styles + pretty errors on auth page

--- front/just-react-simpler/src/components/Login.js
+++ front/just-react-simpler/src/components/Login.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { useCookies } from 'react-cookie'
 import axios from 'axios'
+import { useErrorQueue } from '../utils/AppContext'
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 const COOKIE_AUTH_NAME = process.env.REACT_APP_COOKIE_AUTH_NAME
+const ALLOW_SIGNUP = process.env.REACT_APP_ALLOW_SIGNUP === 'true'
 
 export default function Login() {
     const [, setCookie] = useCookies()
@@ -11,17 +13,12 @@ export default function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
-    const [error, setError] = useState(null)
-
-    const viewLogIn = (status) => {
-        setError(null)
-        setIsLogIn(status)
-    }
+    const { addError } = useErrorQueue()
 
     const handleSubmit = async (e, endpoint) => {
         e.preventDefault()
         if (!isLogIn && password !== confirmPassword) {
-            setError('Passwords don\'t match :(')
+            addError('Passwords don\'t match :(')
             return
         }
         const url = `${BACKEND_URL}/${endpoint}`
@@ -42,51 +39,51 @@ export default function Login() {
             })
             .catch(e => {
                 if (e.response) {
-                    setError(e.response.data)
+                    addError(e.response.data)
                 } else
                     console.log(e)
             })
     }
 
     return (
-        <div className="auth">
-            <form className="auth-form">
+        <div className='auth'>
+            <div className='auth-options'>
+                <button
+                    onClick={() => setIsLogIn(true)}
+                    className={`auth-option ${!isLogIn && 'dull'}`}
+                >Log In</button>
+                <button
+                    onClick={() => setIsLogIn(false)}
+                    className={`auth-option ${isLogIn && 'dull'}`}
+                    disabled={!ALLOW_SIGNUP}
+                >Sign Up</button>
+            </div>
+            <form className='auth-form'>
                 <input
-                    className="auth-form-input"
-                    name="username"
-                    placeholder="username"
+                    className='auth-form-input'
+                    name='username'
+                    placeholder='username'
                     onChange={(e) => setUsername(e.target.value)}
                 />
                 <input
-                    className="auth-form-input"
-                    name="password"
-                    type="password"
-                    placeholder="password"
+                    className='auth-form-input'
+                    name='password'
+                    type='password'
+                    placeholder='password'
                     onChange={(e) => setPassword(e.target.value)}
                 />
                 {!isLogIn &&
                     <input
-                        className="auth-input"
-                        type="password"
-                        placeholder="confirm password"
+                        className='auth-form-input'
+                        type='password'
+                        placeholder='confirm password'
                         onChange={(e) => setConfirmPassword(e.target.value)}
                     />}
-                <button type="submit"
+                <button type='submit'
                     onClick={(e) => handleSubmit(e, isLogIn ? 'login' : 'signup')}>
-                    {isLogIn ? "login" : "sign up"}
+                    {isLogIn ? 'enter' : 'create'}
                 </button>
-                {error && <div className="input-error">{error}</div>}
             </form>
-            <div className="auth-options">
-                <button
-                    onClick={() => viewLogIn(true)}
-                    className={isLogIn ? "auth-option" : "auth-option dull"}
-                >Log In</button>
-                <button
-                    onClick={() => viewLogIn(false)}
-                    className={isLogIn ? "auth-option dull" : "auth-option"}
-                >Sign Up</button>
-            </div>
         </div>
     )
 }
