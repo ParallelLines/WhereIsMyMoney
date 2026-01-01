cd6daaaf93ea9b839263312d7c9199b6f3460d31
ParallelLines <tishkane@yandex.ru>
2026-01-01 13:59:16 +0100

    back: return currencies not only according to last used in expenses, but also last used in regular expenses

--- back/express/sql/currenciesGetAllOrdered.sql
+++ back/express/sql/currenciesGetAllOrdered.sql
@@ -1,10 +1,24 @@
-SELECT c.name, c.symbol
+SELECT
+    c.name,
+    c.symbol
 FROM currencies c
 LEFT JOIN (
     SELECT e.currency
     FROM expenses e
-    WHERE user_id = $1
+    WHERE e.user_id = $1
     ORDER BY e.date DESC
     LIMIT 1
-) last_expense_currency ON c.name = last_expense_currency.currency
-ORDER BY last_expense_currency.currency IS NULL, c.name
+) last_expense_currency
+    ON c.name = last_expense_currency.currency
+LEFT JOIN (
+    SELECT r.currency
+    FROM regulars r
+    WHERE r.user_id = $1
+    ORDER BY r.start_date DESC
+    LIMIT 1
+) last_regular_expense_currency
+    ON c.name = last_regular_expense_currency.currency
+ORDER BY
+    (last_expense_currency.currency IS NULL),
+    (last_regular_expense_currency.currency IS NULL),
+    c.name
