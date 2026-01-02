6d61bf77aa40b1a88e5344eb9c106b0b247cf9e6
ParallelLines <tishkane@yandex.ru>
2026-01-02 20:04:43 +0100

    back: no more sign ups and user routes

--- back/express/app.js
+++ back/express/app.js
@@ -6,13 +6,13 @@ import './scheduler/regularExpensesScheduler.js'
 import './scheduler/suggestionsUpdateScheduler.js'
 import express from 'express'
 import cors from 'cors'
-import usersRoutes from './routes/users.js'
+// import usersRoutes from './routes/users.js'
 import currenciesRoutes from './routes/currencies.js'
 import categoriesRoutes from './routes/categories.js'
 import expensesRoutes from './routes/expenses.js'
 import regularsRoutes from './routes/regulars.js'
 import loginRoutes from './routes/login.js'
-import signupRoutes from './routes/signup.js'
+// import signupRoutes from './routes/signup.js'
 import suggestionsRoutes from './routes/suggestions.js'
 
 const PORT = process.env.PORT ?? 3000
@@ -20,13 +20,13 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/users', usersRoutes)
+// app.use('/users', usersRoutes)
 app.use('/currencies', currenciesRoutes)
 app.use('/categories', categoriesRoutes)
 app.use('/expenses', expensesRoutes)
 app.use('/regulars', regularsRoutes)
 app.use('/suggestions', suggestionsRoutes)
-app.use('/signup', signupRoutes)
+// app.use('/signup', signupRoutes)
 app.use('/login', loginRoutes)
 
 app.listen(PORT, () => {
