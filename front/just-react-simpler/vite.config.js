8416b3291a9029adb9856f2a5da4abb091d0ba32
ParallelLines <tishkane@yandex.ru>
2026-04-20 21:38:34 +0200

    front: probably a fix for path resolving

--- front/just-react-simpler/vite.config.js
+++ front/just-react-simpler/vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    const publicUrl = env.PUBLIC_URL || '/'
+
+    const base =
+        publicUrl === '/'
+            ? '/'
+            : `/${publicUrl.replace(/^\/|\/$/g, '')}/`
+
+    return {
+        plugins: [react()],
+        base,
+    }
 })
