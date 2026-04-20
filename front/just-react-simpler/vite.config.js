a9dec6717436aa2e93b577ff476b42917bcf4422
Roman Garanin <git@romangaranin.net>
2026-04-20 22:00:22 +0200

    front: simpler base definition
    front: simpler base definition

--- front/just-react-simpler/vite.config.js
+++ front/just-react-simpler/vite.config.js
@@ -1,18 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
+  const base = env.PUBLIC_URL ?? "/";
 
-    const publicUrl = env.PUBLIC_URL || '/'
-
-    const base =
-        publicUrl === '/'
-            ? '/'
-            : `/${publicUrl.replace(/^\/|\/$/g, '')}/`
-
-    return {
-        plugins: [react()],
-        base,
-    }
-})
+  return {
+    plugins: [react()],
+    base,
+  };
+});
