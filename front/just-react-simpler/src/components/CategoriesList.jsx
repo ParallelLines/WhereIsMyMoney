810cc2287d8b0905a690bb9652043a46bb1557e7
ParallelLines <tishkane@yandex.ru>
2026-06-18 17:46:41 +0200

    front: now scroll to the top of the categories list before creating a new one with [+] button

--- front/just-react-simpler/src/components/CategoriesList.jsx
+++ front/just-react-simpler/src/components/CategoriesList.jsx
@@ -1,19 +1,27 @@
 import Category from './Category'
 import { useFetchCategories } from '../utils/reactQueryHooks'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import CategoryForm from './CategoryForm'
 
 export default function CategoriesList() {
     const [createMode, setCreateMode] = useState(false)
+    const listToScroll = useRef(null)
+
+    const handleCreate = () => {
+        if (listToScroll.current) {
+            listToScroll.current.scrollTop = 0
+        }
+        setCreateMode(true)
+    }
 
     const query = useFetchCategories()
 
     return (
         <div className='categories-list'>
             <div className='list-ctrls'>
-                <button onClick={() => setCreateMode(true)}>+</button>
+                <button onClick={handleCreate}>+</button>
             </div>
-            <div className='list-column'>
+            <div className='list-column' ref={listToScroll}>
                 {createMode &&
                     <CategoryForm
                         level="1"
